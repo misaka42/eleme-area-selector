8fc9eb29a9be7c5d77cc4fd953418d6d3f14e7d9
Misaka <ppq1991@gmail.com>
2016-07-25 16:14:19 +0800

    patch

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@
 
   var DEFAULT_CONFIG = {
     origin: '/api/other/filter/', // api url
-    styleCDN: '//npm.elemecdn.com/eleme-area-selector@0.1.0/dist/style.css', // style from CDN
+    styleCDN: '//npm.elemecdn.com/eleme-area-selector@0.1.7/dist/style.css', // style from CDN
     typeMap: {
       '交易平台BU': { id: 'bu', params: { time: 'hour' } },
       '红包': { id: 'redReward' },
