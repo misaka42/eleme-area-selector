0184921d2f9a2e9c3d9fd1326eb77cbeb6e51fc5
Misaka <ppq1991@gmail.com>
2016-09-21 17:11:19 +0800

    patch

--- src/index.js
+++ src/index.js
@@ -46,6 +46,8 @@
       this.el.innerHTML = this.config.loadingMessage[0];
       this.$initCache();
 
+      this.$refreshModel = throttle(this._refreshModel, 50);
+
       // has load ?
       if (!document.querySelector('[data-id=EAS-Style]')) {
         var style = createElement('link', { rel: 'stylesheet', href: this.config.style }, { id: 'EAS-Style' });
@@ -418,7 +420,7 @@
       return model;
     },
 
-    $refreshModel: function() {
+    _refreshModel: function() {
       var level = this.currentLevel;
       this.refs.tags.innerHTML = this.model.map(function(item) {
         var className = 'eas-tag';
@@ -431,6 +433,8 @@
       this.$setCache();
       if (this.config.onChange) { this.config.onChange() }
     },
+
+    $refreshModel: null,
 
     $initCache: function() {
       var cache = window.localStorage.getItem(this.config.cacheKey);
@@ -586,6 +590,48 @@
       }
     }
   }
+
+  function throttle(func, wait, options) {
+   var timeout, context, args, result;
+   var previous = 0;
+   if (!options) options = {};
+
+   var later = function() {
+     previous = options.leading === false ? 0 : new Date().getTime();
+     timeout = null;
+     result = func.apply(context, args);
+     if (!timeout) context = args = null;
+   };
+
+   var throttled = function() {
+     var now = new Date().getTime();
+     if (!previous && options.leading === false) previous = now;
+     var remaining = wait - (now - previous);
+     context = this;
+     args = arguments;
+     if (remaining <= 0 || remaining > wait) {
+       if (timeout) {
+         clearTimeout(timeout);
+         timeout = null;
+       }
+       previous = now;
+       result = func.apply(context, args);
+       if (!timeout) context = args = null;
+     } else if (!timeout && options.trailing !== false) {
+       timeout = setTimeout(later, remaining);
+     }
+     return result;
+   };
+
+   throttled.cancel = function() {
+     clearTimeout(timeout);
+     previous = 0;
+     timeout = context = args = null;
+   };
+
+   return throttled;
+ };
+
   /* ---- utils end ---- */
 
   /* ---- exports ---- */
