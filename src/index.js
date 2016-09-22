2b1d59cb681938e2f076f08208497663d0fb4a46
Misaka <ppq1991@gmail.com>
2016-09-22 17:28:43 +0800

    patch

--- src/index.js
+++ src/index.js
@@ -46,8 +46,6 @@
       this.el.innerHTML = this.config.loadingMessage[0];
       this.$initCache();
 
-      this.$refreshModel = throttle(this._refreshModel, 50);
-
       // has load ?
       if (!document.querySelector('[data-id=EAS-Style]')) {
         var style = createElement('link', { rel: 'stylesheet', href: this.config.style }, { id: 'EAS-Style' });
@@ -231,9 +229,8 @@
           }
         }
       }
-      forEach(model.reverse(), function(item) {
-        $self.$selectItem(item);
-      });
+
+      this.$selectItem(model.reverse());
 
       if (this.config.onReady) { this.config.onReady() }
     },
@@ -383,6 +380,13 @@
     },
 
     $selectItem: function(item) {
+      if (Array.isArray(item)) {
+        this.model = item;
+        this.$setCurrentLevel(item[0].level);
+        this.$refreshModel();
+        return;
+      }
+
       if (!this.model.some(function(i) { return i.i === item.i && i.level === item.level; })) {
         this.model.unshift(item);
         this.$setCurrentLevel(item.level);
@@ -420,7 +424,7 @@
       return model;
     },
 
-    _refreshModel: function() {
+    $refreshModel: function() {
       var level = this.currentLevel;
       this.refs.tags.innerHTML = this.model.map(function(item) {
         var className = 'eas-tag';
@@ -433,8 +437,6 @@
       this.$setCache();
       if (this.config.onChange) { this.config.onChange() }
     },
-
-    $refreshModel: null,
 
     $initCache: function() {
       var cache = window.localStorage.getItem(this.config.cacheKey);
@@ -590,47 +592,6 @@
       }
     }
   }
-
-  function throttle(func, wait, options) {
-   var timeout, context, args, result;
-   var previous = 0;
-   if (!options) options = {};
-
-   var later = function() {
-     previous = options.leading === false ? 0 : new Date().getTime();
-     timeout = null;
-     result = func.apply(context, args);
-     if (!timeout) context = args = null;
-   };
-
-   var throttled = function() {
-     var now = new Date().getTime();
-     if (!previous && options.leading === false) previous = now;
-     var remaining = wait - (now - previous);
-     context = this;
-     args = arguments;
-     if (remaining <= 0 || remaining > wait) {
-       if (timeout) {
-         clearTimeout(timeout);
-         timeout = null;
-       }
-       previous = now;
-       result = func.apply(context, args);
-       if (!timeout) context = args = null;
-     } else if (!timeout && options.trailing !== false) {
-       timeout = setTimeout(later, remaining);
-     }
-     return result;
-   };
-
-   throttled.cancel = function() {
-     clearTimeout(timeout);
-     previous = 0;
-     timeout = context = args = null;
-   };
-
-   return throttled;
- };
 
   /* ---- utils end ---- */
 
